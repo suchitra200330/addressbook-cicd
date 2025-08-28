7a142c3904daf72255df2ea1ef9a52163c43fe5c
suchitra <suchitrasimhadati@gmail.com>
2025-08-28 18:13:23 +0530

    Add Terraform scripts for EC2 creation

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "ap-south-1"   # change if needed
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "key par"
+}
