2a984ff81a23906a550657b3a5e5b1df916f65fd
Suchitra <suchitrasimhadati@gmail.com>
2025-08-27 23:53:58 -0700

    Initial commit of Terraform project

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicrop/aws"
+      version= "~> 5.0"
+     }
+   }
+}
+provider "aws" {
+  region = var.region
+}
+
+# Get Amazon Linux 2 AMI
+data "aws_ami" "amzn2" {
+  most_recent = true
+  owners      = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Jenkins server
+resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.amzn2.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
+
+# App server
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.amzn2.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "app-server"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "key-pair"
+}
+
